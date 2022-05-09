436ba0d2e0514bb4edbbb75993956f98c800f50e
Odell Purifoy <opurifoy@springhealth.com>
2022-05-09 13:11:53 -0400

    Added queries for post
    -Added query to get all posts
    -Added query to get single posts
    -Added user to PostType

--- .history/app/graphql/types/post_type_20220509131059.rb
+++ .history/app/graphql/types/post_type_20220509131059.rb
@@ -6,7 +6,7 @@ module Types
     field :user_id, Integer, null: false
     field :title, String
     field :body, String
-    field :user, Types::UserType, null: false
+    field :user, Types::UserType, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
