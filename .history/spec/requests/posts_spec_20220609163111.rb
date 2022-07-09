9253ed51f81180283b0d93024aacbb674e2e7bde
Odell Purifoy <opurifoy@springhealth.com>
2022-07-09 17:04:36 -0400

    Moved controller specs to dedicated folder

--- .history/spec/requests/posts_spec_20220609163111.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Posts API', type: :request do
-  it 'should return all posts' do
-    get '/api/v1/posts'
-
-    expect(response).to have_http_status(:success)
-  end
-end
